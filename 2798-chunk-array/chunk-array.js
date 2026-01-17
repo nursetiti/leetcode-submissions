01ea3ff9e5247d6a6adae5036545dfb98a5a020d
Alade Nosirat <nosiratalade1@gmail.com>
2026-01-17 02:57:45 +0100

    Time: 50 ms (48.84%) | Memory: 55.3 MB (95.18%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -4,6 +4,13 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    const result = _.chunk(arr, size);
+    const result = []
+    
+    for (let i = 0; i < arr.length; i += size) {
+        const chunk = arr.slice(i, i + size);
+        result.push(chunk);
+    }
+
     return result;
 };
+
