bae46a0444bb308335df607781d4a3c46f4cdaf3
Alade Nosirat <nosiratalade1@gmail.com>
2026-02-06 03:58:25 +0100

    Time: 0 ms (100.00%) | Memory: 13.1 MB (79.60%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return ([seen[target-num], i])
+            elif num not in seen:
+                seen[num] = i
+
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
