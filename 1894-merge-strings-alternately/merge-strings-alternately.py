d02ee32b470c3afdc426abab4d75527a970dcbad
Alade Nosirat <nosiratalade1@gmail.com>
2026-09-04 08:13:00 +0100

    Time: 15 ms (67.44%) | Memory: 12.4 MB (21.35%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -8,15 +8,22 @@ class Solution(object):
         merged =[]
         # notInList=[]
 
-        for i, char1 in enumerate(word1):
-            for j, char2 in enumerate(word2):
-                if i==j:
-                    merged.append(char1) 
-                    merged.append(char2)
-        if len(word1) > len(word2):
-            merged.extend(word1[len(word2):])
-        elif len(word2) > len(word1):
-            merged.extend(word2[len(word1):])
+        # for i, char1 in enumerate(word1):
+        #     for j, char2 in enumerate(word2):
+        #         if i==j:
+        #             merged.append(char1) 
+        #             merged.append(char2)
+        # if len(word1) > len(word2):
+        #     merged.extend(word1[len(word2):])
+        # elif len(word2) > len(word1):
+        #     merged.extend(word2[len(word1):])
+
+        for i in range(min(len(word1), len(word2))):
+            merged.append(word1[i])
+            merged.append(word2[i])
+
+        merged.extend(word1[len(word2):])
+        merged.extend(word2[len(word1):])
         
         merged ="".join(merged)
         return merged
